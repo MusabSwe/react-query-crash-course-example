c141aa38fff2314c9b181be649f1289e705d153f
Musab <malqarninet@gmail.com>
2023-04-26 16:17:25 +0300

    useQueryClient to display any mutation per refetch refresh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from "@tanstack/react-query"
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
 
 const posts = [
     { id: 1, title: "Post 1" },
@@ -6,7 +6,9 @@ const posts = [
 ]
 
 function App() {
-    console.log(posts);
+    // used to access the states in the queries and handle posts when I click add a post
+    console.log(posts)
+    const queryClient = useQueryClient();
     // UseQuery syntax 
     const postQuery = useQuery({
         // key of function
@@ -14,14 +16,20 @@ function App() {
         // promise function such as fetch data from server
         queryFn: () => wait(1000).then(() => [...posts]),
     })
-    
-    // UseMutation to change fetched posts
+
+    // UseMutation to change fetched query
     const newPostMutation = useMutation({
         mutationFn: title => {
             return wait(1000).then(() =>
                 posts.push({ id: crypto.randomUUID(), title })
             )
-        }
+        },
+        // if I have a successful mutation of data what should I do
+        onSuccess: () => {
+            // used to display mutation data in the page for each refresh in the 
+            // reactQueryDevtools
+            queryClient.invalidateQueries(["posts"])
+        },
     })
 
     // UseQuery validation
@@ -34,7 +42,8 @@ function App() {
         {postQuery.data.map(post => (
             <div key={post.id}>{post.title}</div>
         ))}
-        <button onClick={() => newPostMutation.mutate("New Post")}>
+        {/* invokbe mutation to add posts */}
+        <button disabled={newPostMutation.isLoading} onClick={() => newPostMutation.mutate("New Post")}>
             Add a Post
         </button>
     </div>
