c6b6de96896d106a8611e09f079c21753db2adfb
Musab <malqarninet@gmail.com>
2023-04-26 12:58:48 +0300

    Build the from scratch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,53 +1,45 @@
-import { useQueryClient } from "@tanstack/react-query"
-import { useState } from "react"
-import { getPost } from "./api/posts"
-import { CreatePost } from "./CreatePost"
-import Post from "./Post"
-import { PostListInfinite } from "./PostListInfinite"
-import { PostListPaginated } from "./PostListPaginated"
-import PostsList1 from "./PostsList1"
-import PostsList2 from "./PostsList2"
+import { useQuery, useMutation } from "@tanstack/react-query"
 
-export default function App() {
-  const [currentPage, setCurrentPage] = useState(<PostsList1 />)
-  const queryClient = useQueryClient()
+const posts = [
+    { id: 1, title: "Post 1" },
+    { id: 2, title: "Post 2" },
+]
 
-  function onHoverPostOneLink() {
-    queryClient.prefetchQuery({
-      queryKey: ["posts", 1],
-      queryFn: () => getPost(1),
+function App() {
+    console.log(posts);
+    // UseQuery syntax 
+    const postQuery = useQuery({
+        // key of function
+        queryKey: ["posts"],
+        // promise function such as fetch data from server
+        queryFn: () => wait(1000).then(() => [...posts]),
     })
-  }
-
-  return (
-    <div>
-      <button onClick={() => setCurrentPage(<PostsList1 />)}>
-        Posts List 1
-      </button>
-      <button onClick={() => setCurrentPage(<PostsList2 />)}>
-        Posts List 2
-      </button>
-      <button
-        onMouseEnter={onHoverPostOneLink}
-        onClick={() => setCurrentPage(<Post id={1} />)}
-      >
-        First Post
-      </button>
-      <button
-        onClick={() =>
-          setCurrentPage(<CreatePost setCurrentPage={setCurrentPage} />)
+    const newPostMutation = useMutation({
+        mutationFn: title => {
+            return wait(1000).then(() =>
+                posts.push({ id: crypto.randomUUID(), title })
+            )
         }
-      >
-        New Post
-      </button>
-      <button onClick={() => setCurrentPage(<PostListPaginated />)}>
-        Post List Paginated
-      </button>
-      <button onClick={() => setCurrentPage(<PostListInfinite />)}>
-        Post List Infinite
-      </button>
-      <br />
-      {currentPage}
+    })
+
+    // UseQuery validation
+    if (postQuery.isLoading) return <h1>Loading...</h1>
+    if (postQuery.isError) {
+        return <pre>{JSON.stringify(postQuery.error)}</pre>
+    }
+    // UseQuery returned data
+    return <div>
+        {postQuery.data.map(post => (
+            <div key={post.id}>{post.title}</div>
+        ))}
+        <button onClick={() => newPostMutation.mutate("New Post")}>
+            Add a Post
+        </button>
     </div>
-  )
 }
+
+function wait(duration) {
+    return new Promise(resolve => setTimeout(resolve, duration))
+}
+
+export default App
